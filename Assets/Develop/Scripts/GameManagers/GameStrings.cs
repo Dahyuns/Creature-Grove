90235b5a6dd1ae7500fb23bce9e2cb4121d1e238
Lee Dahyun <cloud20211@naver.com>
2024-08-01 21:13:36 +0900

    Add GameStrings static class for frequently used strings

---
+++ Assets/Develop/Scripts/GameManagers/GameStrings.cs
@@ -0,0 +1,10 @@
+namespace CreatureGrove
+{
+    public static class GameStrings
+    {
+        public const string FirePoint = "FirePoint";
+        public const string Inventory = "Inventory";
+        public const string WeaponTag = "Weapon";
+
+    }
+}
