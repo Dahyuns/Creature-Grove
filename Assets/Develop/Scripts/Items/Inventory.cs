50c701acd8a0fec1ed7b1e85b29963d271b748ac
Lee Dahyun <cloud20211@naver.com>
2024-08-09 23:33:15 +0900

    Implement inventory save/delete methods

--- Assets/Develop/Scripts/Items/Inventory.cs
+++ Assets/Develop/Scripts/Items/Inventory.cs
@@ -1,45 +1,43 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace CreatureGrove
 {
-    public class Inventory : MonoBehaviour, IFieldItemManager, IItemManager
+    public class Inventory : MonoBehaviour
     {
         #region 싱글턴
+        private static Inventory instance;
+        public static Inventory Instance
+        {
+            get { return instance; }
+        }
 
-
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+                return;
+            }
+            DestroyImmediate(gameObject);
+        }
         #endregion
 
+        // 인벤토리 저장칸
+        private List<Item> items;
+        public List<Item> Items { get { return items; } }
 
         // 인벤토리에 등록
-        public void addToInventory(ItemType itemType)
+        public void addToInventory(Item item)
         {
-
+            items.Add(item);
         }
         
         // 인벤토리에서 삭제
-        public void removeFromInventory(ItemType itemType) 
+        public void removeFromInventory(Item item) 
         {
-                    
-        }
-
-
-        // [IFieldItemManager]
-        public void PerformAction(FieldAction action, ItemType itemType)
-        {
-
-        }
-
-        // [IItemManager]
-        // 장착
-        public void Equip()
-        {
-
-        }
-
-        // 해제
-        public void Unequip()
-        {
-
+            items.Remove(item);
         }
     }
 }
