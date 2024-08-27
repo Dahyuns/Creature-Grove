a6729af209f66cea5378890a15ab9d849f12a559
Lee Dahyun <cloud20211@naver.com>
2024-08-27 23:14:10 +0900

    Organize all prefabs by type

--- Assets/Develop/Scripts/Field/Wilderness/Wilderness.cs
+++ Assets/Develop/Scripts/Field/Wilderness/Wilderness.cs
@@ -8,11 +8,6 @@ namespace CreatureGrove
         Enemy      // 몬스터
     }
 
-    enum NatureObjType
-    {
-        Grass, Tree, Rock
-    }
-
     // 필드 - 야생
     // 나무, 잡초 생성 및 관리
     public class Wilderness : MonoBehaviour
@@ -40,6 +35,7 @@ namespace CreatureGrove
         [SerializeField] private GameObject[] grasses;
         [SerializeField] private GameObject[] trees;
         [SerializeField] private GameObject[] rocks;
+        [SerializeField] private GameObject[] mushrooms;
         [SerializeField] private GameObject parent;
 
         // 한칸당 유닛의 개수
@@ -47,11 +43,10 @@ namespace CreatureGrove
         [SerializeField] private float grassScale = 1;
         [SerializeField] private float treeScale = 1;
         [SerializeField] private float rockScale = 1;
+        [SerializeField] private float mushroomScale = 1;
 
         // sin함수 그래프 폭(주기) 간격 (커질수록 촘촘해짐, 기본은 1)
         [SerializeField] private float Mult = 1;
-
-        private GameObject sineObj;
 
         private void Start()
         {
@@ -69,16 +64,17 @@ namespace CreatureGrove
                     // 해당 칸이 야생이라면
                     if (array[x, y] == 0)
                     {
-                        InstantiateSineWaveObject(GridManager.Instance.cellSize, NatureObjType.Grass, grassScale, x, y, 0);
-                        InstantiateSineWaveObject(GridManager.Instance.cellSize, NatureObjType.Tree,  treeScale, x, y, 2);
-                        InstantiateSineWaveObject(GridManager.Instance.cellSize, NatureObjType.Rock,  rockScale, x, y, 4);
+                        InstantiateSineWaveObject(GridManager.Instance.cellSize, mushrooms, mushroomScale, x, y, 0);
+                        InstantiateSineWaveObject(GridManager.Instance.cellSize, grasses, grassScale, x, y, 2);
+                        InstantiateSineWaveObject(GridManager.Instance.cellSize, trees,  treeScale, x, y, 3);
+                        InstantiateSineWaveObject(GridManager.Instance.cellSize, rocks,  rockScale, x, y, 4);
                     }
                 }
             }
         }
 
         // 사인 그래프를 활용하여 오브젝트를 생성 (한칸의 크기, 생성할 오브젝트, x칸 번호, y칸 번호, 랜덤 시드값 (2파이 안으로))
-        private void InstantiateSineWaveObject(float cellSize, NatureObjType ntype, float scale, int x, int y, int seed)
+        private void InstantiateSineWaveObject(float cellSize, GameObject[] array, float scale, int x, int y, int seed)
         {
             // 위에서 아래 x = sin(y)
             // 한 단위당 거리
@@ -91,20 +87,7 @@ namespace CreatureGrove
                 // 진폭, 파형의 '높이'(=cellSize의 반) * SIN함수 (드롭 간격 * x값)     + 원점이동값(=cellSize의 반)
                 float numY = (cellSize / 2) * Mathf.Sin(Mult * numX + seed) - cellSize / 2;
 
-                switch (ntype)
-                {
-                    case NatureObjType.Grass:
-                        sineObj = Instantiate(RandomGrassObj(), parent.transform);
-                        break;
-
-                    case NatureObjType.Tree:
-                        sineObj = Instantiate(RandomTreeObj(), parent.transform);
-                        break;
-
-                    case NatureObjType.Rock:
-                        sineObj = Instantiate(RandomRockObj(), parent.transform);
-                        break;
-                }
+                GameObject sineObj = Instantiate(RandomObj(array), parent.transform);
 
                 float newGrassX = this.transform.position.x + x * cellSize + numX;
                 float newGrassY = this.transform.position.y + y * cellSize + numY;
@@ -114,27 +97,12 @@ namespace CreatureGrove
             }
         }
 
-        #region 인스펙터 창 내 배열에서 랜덤한 오브젝트 반환
-        private GameObject RandomGrassObj()
+        // 인스펙터 창 내 배열에서 랜덤한 오브젝트 반환
+        private GameObject RandomObj(GameObject[] array)
         {
-            int i = Random.Range(0, grasses.Length);
+            int i = Random.Range(0, array.Length);
 
-            return grasses[i];
+            return array[i];
         }
-
-        private GameObject RandomTreeObj()
-        {
-            int i = Random.Range(0, trees.Length);
-
-            return trees[i];
-        }
-
-        private GameObject RandomRockObj()
-        {
-            int i = Random.Range(0, rocks.Length);
-
-            return rocks[i];
-        }
-        #endregion
     }
 }
